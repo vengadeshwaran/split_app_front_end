bf20fcf35906551784209bc567892fb63f86021b
Vengadeshwaran <vengadeshwaranganesan@gmail.com>
2026-06-29 10:27:07 +0400

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -15,7 +15,7 @@ export default defineConfig(({ mode }) => {
       tailwindcss(),
     ],
     define: {
-      'import.meta.env.BACKEND_URL': JSON.stringify(env.BACKEND_URL || 'http://localhost:5000'),
+      'import.meta.env.BACKEND_URL': JSON.stringify(env.BACKEND_URL || 'https://split-app-back-end.onrender.com'),
     },
     server: {
       host: true,
@@ -23,7 +23,7 @@ export default defineConfig(({ mode }) => {
       open: true,
       proxy: {
         "/api": {
-          target: env.BACKEND_URL || 'http://localhost:5000',
+          target: env.BACKEND_URL || 'https://split-app-back-end.onrender.com',
           changeOrigin: true,
           secure: false,
         },
